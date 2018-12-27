0a93f0d2b9892a7d93a82795d9bc5f93fb915762
mehdibalbali <mehdibalbali@yahoo.fr>
2018-12-27 18:14:41 +0100

    initialisation de l'interface

---
+++ Main.java
@@ -0,0 +1,23 @@
+package toDoList;
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+		}
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			Parent root=FXMLLoader.load(getClass().getResource("window.fxml"));
+			primaryStage.setTitle("ToDo List");
+			primaryStage.setScene(new Scene(root,900,500));
+			primaryStage.show();
+}
+}
