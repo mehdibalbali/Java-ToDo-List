b4a1c4e66114d59f86f76078ce16d2a9aa467268
mehdibalbali <mehdibalbali@yahoo.fr>
2018-12-28 02:07:11 +0100

    add some TodoList examples
    just add some items to the todoList
    the program can show details

--- Controller.java
+++ Controller.java
@@ -1,5 +1,50 @@
 package toDoList;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import toDoList.data.*;
 
 public class Controller {
 
+	private List<TodoItems> todoItems;
+	
+	@FXML
+	private ListView list;
+	@FXML
+	private TextArea itemDetails;
+	
+	public void initialize() {
+		TodoItems item1=new TodoItems("Mail to friend","you must send an emaill",LocalDate.of(2018, Month.FEBRUARY	, 21));
+		TodoItems item2=new TodoItems("go to swim","you must go to miscine",LocalDate.of(2018, Month.JULY	, 1));
+		TodoItems item3=new TodoItems("visit friend","you have a date with friend",LocalDate.of(2018, Month.MARCH	, 2));
+		TodoItems item4=new TodoItems("clean the howse ","you must send an emaill",LocalDate.of(2018, Month.FEBRUARY	, 21));
+		TodoItems item5=new TodoItems("pick up dry cleaning","Clothes must be ready ",LocalDate.of(2018, Month.APRIL	, 21));
+		
+		todoItems=new ArrayList<TodoItems>();
+		
+		todoItems.add(item1);
+		todoItems.add(item2);
+		todoItems.add(item3);
+		todoItems.add(item4);
+		todoItems.add(item5);
+		
+		list.getItems().setAll(todoItems);	
+		list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	}
+	
+	public void handleClicListView() {
+		TodoItems item= (TodoItems) list.getSelectionModel().getSelectedItem();
+		System.out.println("Selected the item "+item);
+		StringBuilder sb=new StringBuilder(item.getDetails());
+		sb.append("\n\n\n");
+		sb.append("-------------\n");
+		sb.append(item.getDeadline().toString());
+		itemDetails.setText(sb.toString());
+	}
 }
---
+++ data/TodoItems.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package toDoList.data;
+
+import java.time.LocalDate;
+
+/**
+ * @author mehdibalbali
+ *
+ */
+public class TodoItems {
+	private String description ;
+	private String details;
+	private LocalDate deadline;
+	public TodoItems(String description, String details, LocalDate localDate) {
+		this.description = description;
+		this.details = details;
+		this.deadline = localDate;
+	}
+	/**
+	 * @param description2
+	 * @param details2
+	 * @param of
+	 */
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public LocalDate getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(LocalDate deadline) {
+		this.deadline = deadline;
+	}
+	@Override
+	public String toString() {
+		return  description;
+	}
+	
+	
+	
+}
